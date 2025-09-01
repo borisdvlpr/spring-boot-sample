0b8fcd7b13e966c864f5cb40136f80c8151ff8e9
borisdvlpr <eduffmiranda@hotmail.com>
2025-09-02 00:30:24 +0100

    feat: add and implement createCategory method for the CategoryService

--- src/main/java/com/borisdvlpr/sample/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/borisdvlpr/sample/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.borisdvlpr.sample.service.impl;
 import com.borisdvlpr.sample.domain.entities.Category;
 import com.borisdvlpr.sample.repository.CategoryRepository;
 import com.borisdvlpr.sample.service.CategoryService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,15 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<Category> listCategories() {
         return categoryRepository.findAllWithPostCount();
+    }
+
+    @Override
+    @Transactional
+    public Category createCategory(Category category) {
+        if (categoryRepository.existsByNameIgnoreCase(category.getName())) {
+            throw new IllegalArgumentException("Category already exists with name: " + category.getName());
+        }
+
+        return categoryRepository.save(category);
     }
 }
