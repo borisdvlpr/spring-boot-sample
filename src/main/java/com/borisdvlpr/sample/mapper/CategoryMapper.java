b068e96142c714cf482e38c2c2c4ea7bf1e54aa9
borisdvlpr <eduffmiranda@hotmail.com>
2025-09-01 07:45:39 +0100

    feat: create CategoryMapper for Category to CategoryDTO conversion

---
+++ src/main/java/com/borisdvlpr/sample/mapper/CategoryMapper.java
@@ -0,0 +1,29 @@
+package com.borisdvlpr.sample.mapper;
+
+import com.borisdvlpr.sample.domain.PostStatus;
+import com.borisdvlpr.sample.domain.dto.CategoryDTO;
+import com.borisdvlpr.sample.domain.entities.Category;
+import com.borisdvlpr.sample.domain.entities.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CategoryMapper {
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
+    CategoryDTO toDTO(Category category);
+
+    @Named("calculatePostCount")
+    default long calculatePostCount(List<Post> posts) {
+        if(posts == null) {
+            return 0;
+        }
+
+        return posts.stream()
+                .filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
+}
