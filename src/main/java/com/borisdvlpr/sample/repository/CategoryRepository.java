526bf819ad748da19245c36b1e45bee03b580f96
borisdvlpr <eduffmiranda@hotmail.com>
2025-09-01 02:14:17 +0100

    feat: create Category repository

---
+++ src/main/java/com/borisdvlpr/sample/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.borisdvlpr.sample.repository;
+
+import com.borisdvlpr.sample.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+}
