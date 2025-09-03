57706f24a71bcd58b67d2be32958b1e5d9776345
borisdvlpr <eduffmiranda@hotmail.com>
2025-09-03 08:55:48 +0100

    fix(controller): add missing request annotation to create category endpoint

--- src/main/java/com/borisdvlpr/sample/controller/CategoryController.java
+++ src/main/java/com/borisdvlpr/sample/controller/CategoryController.java
@@ -9,10 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +29,7 @@ public class CategoryController {
         return ResponseEntity.ok(categories);
     }
 
+    @PostMapping
     public ResponseEntity<CategoryDTO> createCategory(
             @Valid @RequestBody CreateCategoryRequest createCategoryRequest) {
         Category categoryToCreate = categoryMapper.toEntity(createCategoryRequest);
