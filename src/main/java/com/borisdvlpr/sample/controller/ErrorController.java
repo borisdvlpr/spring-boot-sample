4afb3bf971e0f078fccb38c122cdf52b763e3876
borisdvlpr <eduffmiranda@hotmail.com>
2025-09-03 08:47:22 +0100

    feat: add exception handler controller

---
+++ src/main/java/com/borisdvlpr/sample/controller/ErrorController.java
@@ -0,0 +1,36 @@
+package com.borisdvlpr.sample.controller;
+
+import com.borisdvlpr.sample.domain.dto.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception e) {
+        log.error("Caught exception: ", e);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred")
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalAccessError.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(Exception e) {
+        log.error("Caught exception: ", e);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(e.getMessage())
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
