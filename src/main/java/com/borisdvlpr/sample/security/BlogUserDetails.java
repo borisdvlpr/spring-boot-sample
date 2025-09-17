5ffa24701dc019d4416df19ff0efcdb524a1179b
borisdvlpr <eduffmiranda@hotmail.com>
2025-09-17 23:25:39 +0100

    feat(auth): implement getId method on BlogUserDetails

--- src/main/java/com/borisdvlpr/sample/security/BlogUserDetails.java
+++ src/main/java/com/borisdvlpr/sample/security/BlogUserDetails.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @RequiredArgsConstructor
@@ -28,6 +29,10 @@ public class BlogUserDetails implements UserDetails {
     @Override
     public String getUsername() {
         return user.getEmail();
+    }
+
+    public UUID getId() {
+        return user.getId();
     }
 
     @Override
