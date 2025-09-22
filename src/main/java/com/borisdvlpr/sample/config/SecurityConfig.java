8f33e16632588e5072da87c0887b29282e96d096
borisdvlpr <eduffmiranda@hotmail.com>
2025-09-22 23:05:12 +0100

    feat(auth): enable jwt authentication on SecurityConfig

--- src/main/java/com/borisdvlpr/sample/config/SecurityConfig.java
+++ src/main/java/com/borisdvlpr/sample/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.borisdvlpr.sample.config;
 
+import com.borisdvlpr.sample.repository.UserRepository;
+import com.borisdvlpr.sample.security.BlogUserDetailsService;
+import com.borisdvlpr.sample.security.JwtAuthenticationFilter;
+import com.borisdvlpr.sample.service.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,16 +12,31 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationService authenticationService) {
+        return new JwtAuthenticationFilter(authenticationService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new BlogUserDetailsService(userRepository);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/tags/**").permitAll()
@@ -26,7 +45,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
