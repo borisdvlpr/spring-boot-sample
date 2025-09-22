e7691baf5abfadf0ec08bbd13eaf2f27677dcf89
borisdvlpr <eduffmiranda@hotmail.com>
2025-09-22 23:09:23 +0100

    feat(auth): create tag controller with endpoint to list all tags

---
+++ src/main/java/com/borisdvlpr/sample/controller/TagController.java
@@ -0,0 +1,27 @@
+package com.borisdvlpr.sample.controller;
+
+import com.borisdvlpr.sample.domain.dto.TagResponse;
+import com.borisdvlpr.sample.domain.entities.Tag;
+import com.borisdvlpr.sample.mapper.TagMapper;
+import com.borisdvlpr.sample.service.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/tags")
+@RequiredArgsConstructor
+public class TagController {
+    private final TagService tagService;
+    private final TagMapper tagMapper;
+
+    public ResponseEntity<List<TagResponse>> getAllTags() {
+        List<Tag> tags = tagService.getTags();
+        List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
+
+        return ResponseEntity.ok(tagResponses);
+    }
+}
