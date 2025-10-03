e9430b90866a8949dd1bd930d4049a35cdce19b7
borisdvlpr <eduffmiranda@hotmail.com>
2025-10-04 00:09:42 +0100

    feat(repository): implement PostRepository with queries for status, category, and tags

--- src/main/java/com/borisdvlpr/sample/repository/PostRepository.java
+++ src/main/java/com/borisdvlpr/sample/repository/PostRepository.java
@@ -1,12 +1,22 @@
 package com.borisdvlpr.sample.repository;
 
+import com.borisdvlpr.sample.domain.PostStatus;
+import com.borisdvlpr.sample.domain.entities.Category;
 import com.borisdvlpr.sample.domain.entities.Post;
+import com.borisdvlpr.sample.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
+    List<Post> findAllByStatusAndCategoryAndTagsContaining(PostStatus status, Category category, Tag tag);
 
+    List<Post> findAllByStatusAndCategory(PostStatus status, Category category);
+
+    List<Post> findAllByStatusAndTagsContaining(PostStatus status, Tag tag);
+
+    List<Post> findAllByStatus(PostStatus status);
 }
