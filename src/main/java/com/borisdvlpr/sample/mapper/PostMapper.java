96ce9b50bc2bf78e94686ebb46b2daaf54b0fcd5
borisdvlpr <eduffmiranda@hotmail.com>
2025-10-04 00:13:04 +0100

    feat(dto): add mapper from Post entity to PostDto

---
+++ src/main/java/com/borisdvlpr/sample/mapper/PostMapper.java
@@ -0,0 +1,15 @@
+package com.borisdvlpr.sample.mapper;
+
+import com.borisdvlpr.sample.domain.dto.PostDTO;
+import com.borisdvlpr.sample.domain.entities.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PostMapper {
+    @Mapping(target = "author", source = "author")
+    @Mapping(target = "category", source = "category")
+    @Mapping(target = "tags", source = "tags")
+    PostDTO toDto(Post post);
+}
