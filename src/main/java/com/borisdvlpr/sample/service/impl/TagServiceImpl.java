143034f9480733d7d482d00cea6ad8c09b467235
borisdvlpr <eduffmiranda@hotmail.com>
2025-10-04 19:24:40 +0100

    feat(service): add getTagByIds method to TagService and implementation in TagServiceImpl

--- src/main/java/com/borisdvlpr/sample/service/impl/TagServiceImpl.java
+++ src/main/java/com/borisdvlpr/sample/service/impl/TagServiceImpl.java
@@ -61,4 +61,14 @@ public class TagServiceImpl implements TagService {
     public Tag getTagById(UUID id) {
         return tagRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Tag not found with ID; " + id));
     }
+
+    @Override
+    public List<Tag> getTagByIds(Set<UUID> ids) {
+        List<Tag> foundTags = tagRepository.findAllById(ids);
+        if(foundTags.size() != ids.size()) {
+            throw new EntityNotFoundException("Not all specified tag IDs exist.");
+        }
+
+        return foundTags;
+    }
 }
