cd44d9993df9485b8bff5e05ebeb58cb48d0a77a
borisdvlpr <eduffmiranda@hotmail.com>
2025-10-04 16:55:46 +0100

    fix(controller): add missing mapping annotation to AuthController

--- src/main/java/com/borisdvlpr/sample/controller/AuthController.java
+++ src/main/java/com/borisdvlpr/sample/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.borisdvlpr.sample.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthenticationService authenticationService;
 
+    @PostMapping
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
         UserDetails userDetails = authenticationService.authenticate(
                 loginRequest.getEmail(),
@@ -23,7 +25,6 @@ public class AuthController {
         );
 
         String tokenValue = authenticationService.generateToken(userDetails);
-
         AuthResponse authResponse = AuthResponse.builder()
                 .token(tokenValue)
                 .expiresIn(86400L)
